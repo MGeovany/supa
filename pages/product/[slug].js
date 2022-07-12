8977e5e670f2f3555193eb2a00e412c83c378757
Geovany <marlongeo1999@gmail.com>
2022-07-11 22:09:24 -0600

    🎮 context logic

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -13,7 +13,7 @@ import { useStateContext } from "../../context/StateContext";
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  // const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
 
   const handleBuyNow = () => {
     onAdd(product, qty);
@@ -63,20 +63,20 @@ const ProductDetails = ({ product, products }) => {
           <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
-              {/*  <span className="minus" onClick={decQty}>
+              <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
-              </span> */}
-              {/*   <span className="num">{qty}</span> */}
-              {/*   <span className="plus" onClick={incQty}>
+              </span>
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={incQty}>
                 <AiOutlinePlus />
-              </span> */}
+              </span>
             </p>
           </div>
           <div className="buttons">
             <button
               type="button"
               className="add-to-cart"
-              /*  onClick={() => onAdd(product, qty)} */
+              onClick={() => onAdd(product, qty)}
             >
               Add to Cart
             </button>
@@ -130,7 +130,7 @@ export const getStaticProps = async ({ params: { slug } }) => {
   const product = await client.fetch(query);
   const products = await client.fetch(productsQuery);
 
-  console.log("iam product", product);
+  console.log(product);
 
   return {
     props: { products, product },
